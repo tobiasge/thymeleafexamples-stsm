2c4087f2ae02e6effd04085178e76571e2978f85
Tobias Genannt <t.genannt@scanplus.de>
2017-04-05 14:31:00 +0200

    Using template engine in helth check

--- src/main/java/thymeleafexamples/stsm/web/controller/HealthCheckController.java
+++ src/main/java/thymeleafexamples/stsm/web/controller/HealthCheckController.java
@@ -1,7 +1,5 @@
 package thymeleafexamples.stsm.web.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,12 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HealthCheckController {
 
     @RequestMapping({ "/live" })
-    public ResponseEntity<String> livenessProbe() {
-        return new ResponseEntity<String>(HttpStatus.OK);
+    public String livenessProbe() {
+        return "health";
     }
 
     @RequestMapping({ "/ready" })
-    public ResponseEntity<String> readinessProbe() {
-        return new ResponseEntity<String>(HttpStatus.OK);
+    public String readinessProbe() {
+        return "health";
     }
 }
